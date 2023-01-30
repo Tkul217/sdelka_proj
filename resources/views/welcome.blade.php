f266d30ad05e673e8a82b608018ea81c1e799d0a
Timur <timurcool650@gmail.com>
2023-01-31 00:44:20 +0600

    Final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
                                 <h3>ТОП 3 ЛУЧШИХ СПЕЦИАЛИСТОВ ПО НЕДВИЖИМОСТИ</h3>
                                 <p>Лучшие агенты которые помогут найти для вас подхлдящую недвижимость в кратчайшие сроки!</p>
                                 @foreach($buttons->where('slider_id', 1) as $button)
-                                    <a href="">{{$button->button_name}}</a>
+                                    <a href="https://wa.me/77711018877">{{$button->button_name}}</a>
                                 @endforeach
                             </div>
                             <div class="main_img">
@@ -81,7 +81,7 @@
                                 <h3>ПРОФЕССИОНАЛЬНЫЕ РИЭЛТОРСКИЕ УСЛУГИ ПО АСТАНЕ</h3>
                                 <p>Найдем покупателей за 14 дней и продадим Вашу квартиру на 10% дороже рынка</p>
                                 @foreach($buttons->where('slider_id', 2) as $button)
-                                    <a href="">{{$button->button_name}}</a>
+                                    <a href="https://wa.me/77711018877">{{$button->button_name}}</a>
                                 @endforeach
                             </div>
                             <div class="main_img">
@@ -117,7 +117,7 @@
                         За это время мы зарекомендовали себя как надежное, успешное и современное агентство недвижимости.
                         Мы каждый день улучшаем наш сервис, получая обратную связь от клиентов и партнеров.
                         Наша цель - в недалекой перспективе стать агентством недвижимости №1 в сервисе и заботе о клиенте!</p>
-                    <a href="#">Получить консультацию</a>
+                    <a href="https://wa.me/77711018877">Получить консультацию</a>
                 </div>
             </div>
         </div>
@@ -284,7 +284,7 @@
                 <div class="description">
                     <h3>Мы поможем Вам найти лучшие объекты недвижимости</h3>
                     <p>Мы просеиваем и обрабатываем десятки и сотни объявлений о продаже, избавляя вас от необходимости тратить время впустую на просмотр заведомо несуществующих вариантов или тех, где есть «подводные камни».</p>
-                    <a href="">Связаться</a>
+                    <a href="https://wa.me/77711018877">Связаться</a>
                 </div>
             </div>
         </div>
@@ -312,7 +312,7 @@
                                 <div class="description_second">1% от стоимости недвижимости</div>
                             </div>
                         </div>
-                        <a href="" class="btn_appartment">Выбрать</a>
+                        <a href="https://wa.me/77711018877?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D0%BF%D1%80%D0%B8%D0%BE%D0%B1%D1%80%D0%B5%D1%82%D0%B5%D0%BD%D0%B8%D1%8E%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B%20%D0%BD%D0%B0%20%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%BD%D0%BE%D0%BC%20%D1%80%D1%8B%D0%BD%D0%BA%D0%B5" class="btn_appartment">Выбрать</a>
                     </div>
                 </div>
                 <div class="second_type">
@@ -335,7 +335,7 @@
                                 <div class="description_second">2% от стоимости недвижимости</div>
                             </div>
                         </div>
-                        <a href="" class="btn_appartment">Выбрать</a>
+                        <a href="https://wa.me/77711018877?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D0%BF%D1%80%D0%B8%D0%BE%D0%B1%D1%80%D0%B5%D1%82%D0%B5%D0%BD%D0%B8%D1%8E%20%D0%BA%D0%BE%D0%BC%D0%BC%D0%B5%D1%80%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B9%20%D0%BD%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8%20%D0%BD%D0%B0%20%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%BD%D0%BE%D0%BC%20%D1%80%D1%8B%D0%BD%D0%BA%D0%B5%2C%20%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B4%D0%BE%D0%BC%D0%B0%2C%20%D0%B7%D0%B5%D0%BC%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE%20%D1%83%D1%87%D0%B0%D1%81%D1%82%D0%BA%D0%B0" class="btn_appartment">Выбрать</a>
                     </div>
                 </div>
                 <div class="first_type">
@@ -356,7 +356,7 @@
                                 <div class="description_second">30% от стоимости недвижимости</div>
                             </div>
                         </div>
-                        <a href="" class="btn_appartment">Выбрать</a>
+                        <a href="https://wa.me/77711018877?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D0%B0%D1%80%D0%B5%D0%BD%D0%B4%D0%B5%20%D0%BD%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8" class="btn_appartment">Выбрать</a>
                     </div>
                 </div>
             </div>
@@ -448,7 +448,6 @@
             <div class="content">
                 <div class="title">
                     <h3>Новости недвижимости</h3>
-                    <a href="">Больше новостей</a>
                 </div>
                 <div class="cards">
                     @foreach($blogs as $blog)
@@ -516,7 +515,7 @@
                 <h4>Стань агентом недвижимостии начни зарабатывать уже сейчас</h4>
                 <p>Скорее присоединяйтесь к нашему дружелюбному и профессиональному коллективу<br/>
                     и получи шанс стать финансово независимым</p>
-                <a href="">Оставить заявку</a>
+                <a href="https://wa.me/77711018883">Оставить заявку</a>
             </div>
         </div>
     </section>
